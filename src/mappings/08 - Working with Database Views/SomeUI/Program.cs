2ef43a2db14a6173db61c0731a1801c1c8670b67
Anatoly <anfemail@gmail.com>
2018-07-25 22:24:56 +0300

    Add mappings project

---
+++ src/mappings/08 - Working with Database Views/SomeUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using System.Linq;
+
+namespace SomeUI
+{
+    internal class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        private static void Main(string[] args)
+        {
+            GetStats();
+            Filter();
+            Project();
+        }
+        private static void GetStats()
+        {
+            var stats = _context.SamuraiBattleStats.AsNoTracking().ToList();
+        }
+        private static void Filter()
+        {
+            var stats = _context.SamuraiBattleStats.Where(s => s.SamuraiId == 2).AsNoTracking().ToList();
+        }
+        private static void Project()
+        {
+            var stats = _context.SamuraiBattleStats.AsNoTracking().Select(s => new { s.Name, s.NumberOfBattles }).ToList();
+        }
+    }
+}
